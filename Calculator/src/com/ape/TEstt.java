443bb0750ae33401b008f30e0d4da1f8bec69a7b
jyotimane13901 <netkejyoti9@gmail.com>
2025-09-10 14:31:38 +0530

    create object and method call

--- Calculator/src/com/ape/TEstt.java
+++ Calculator/src/com/ape/TEstt.java
@@ -10,9 +10,19 @@ System.out.println(20-10);
 	public void multi() {
 		System.out.println(30*10);
 	}
-	
+	public void dev(){
+		System.out.println(10/20);
 public static void main(String[] args) {
 	
+	TEstt st = new TEstt ();
+	st.add();
+	st.sub();
+	st.multi();
+	st.dev();
+	
+	
+	
 }
 }
+
 
