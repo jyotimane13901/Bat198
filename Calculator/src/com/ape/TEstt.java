74f3e38e117a63693982c12b501040e453ecf845
jyotimane13901 <netkejyoti9@gmail.com>
2025-09-10 14:14:52 +0530

    Add sub method form Dev2

--- Calculator/src/com/ape/TEstt.java
+++ Calculator/src/com/ape/TEstt.java
@@ -4,7 +4,11 @@ public class TEstt {
 	public void add() {
 		System.out.println(10+20);
 	}
+	public void sub(){
+System.out.println(20-10);
+	}
 public static void main(String[] args) {
 	
 }
 }
+
